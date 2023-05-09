c2c721c9ca760b63643baa168cede72374760c81
Louis T <104893531+LouisT1425@users.noreply.github.com>
2023-05-09 17:33:41 +0200

    Ajout de la classe Monstre, instanciation d'un monstre dans le main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import save_files
+import Monstre
+
 
 Pseudo = input("Veuillez rentrer votre pseudo :")
 
@@ -11,3 +13,12 @@ money = str(money)
 player_data = money + "\n" + mod
 
 save_files.sauvegarde_argent(Pseudo, player_data)
+
+testmnstr_attaque = Monstre.Monstre("Salameche", 10, "Attaque")
+
+testmnstr_defense = Monstre.Monstre("Bulbizarre", 10, "Defense")
+
+a = testmnstr_attaque.show_stats()
+
+print(a)
+
