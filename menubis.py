1b2b80855e02c1921b4b97d77a66480f99dad2b5
Maxine <gras.maxine@gmail.com>
2023-05-15 15:32:41 +0200

    Menubis: affiche une fenetre et un fond d'écrant

--- menubis.py
+++ menubis.py
@@ -1,6 +1,15 @@
 import pygame
 from pygame.locals import *
-
 pygame.init()
 
-fenetre = pygame.display.set_mode((640, 480))
+fenetre = pygame.display.set_mode((640, 480),)
+
+fond = pygame.image.load("fond.jpg.jpg").convert()
+fenetre.blit(fond, (0,0))
+
+pygame.display.flip()
+
+continuer = 1
+
+while continuer:
+    continuer = int(input())
