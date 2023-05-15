734a5f9cc7a636c987c043a8c6f72cf09e2bc24b
Louis T <104893531+LouisT1425@users.noreply.github.com>
2023-05-15 14:25:50 +0200

    Ajoute de la gestion de l'inventaire de monstre dans la classe Player

--- Player.py
+++ Player.py
@@ -1,5 +1,24 @@
+from typing import List, Any
+
+from Monstre import Monstre
+
+
 class Player:
+    monster_list: list[Monstre] = []
+
     def __init__(self, name, balance, mod):
         self.name = name
         self.balance = balance
         self.mod = mod
+
+    def add_monstre(self, Monstre):
+        inside_monster_list = self.monster_list
+        length = len(inside_monster_list)
+        try:
+            if length == 0:
+                inside_monster_list[0] = Monstre
+            elif length != 0:
+                inside_monster_list[length] = Monstre
+        except:
+            print("An error has occurred")
+
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import save_files
 import Monstre
-
+import Player
 
 Pseudo = input("Veuillez rentrer votre pseudo :")
 
-money = 7
+money = 8
 mod = 5
 
 mod = str(mod)
@@ -18,7 +18,17 @@ testmnstr_attaque = Monstre.Monstre("Salameche", 10, "Attaque")
 
 testmnstr_defense = Monstre.Monstre("Bulbizarre", 10, "Defense")
 
-a = testmnstr_attaque.show_stats()
+a = testmnstr_attaque.get_stats()
+b = testmnstr_defense.get_stats()
 
+monstre = [1, 2, 5, 7]
+
+print(len(monstre))
 print(a)
+print(b)
 
+test_joueur = Player.Player("Louis_Thin", money, mod)
+
+test_joueur.add_monstre(testmnstr_attaque)
+
+print(test_joueur.name,test_joueur.monster_list)
