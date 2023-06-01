a813ec9c36d26285db483b7e554f17e5a3101216
Maxine <gras.maxine@gmail.com>
2023-06-01 16:35:00 +0200

    ajout x2

--- menu.py
+++ menu.py
@@ -22,6 +22,8 @@ fenetre.blit(fond, (0, 0)) #on colle sur la fenetre l'image fond et l'angle haut
 def afficher_menu(test, fenetre):
     if test == 1:
         fenetre.blit(fond_menu_boutique, (350, 20))
+        fenetre.blit(mod_x2, (400, 70))
+
 
 
 fond_donee = pygame.image.load("images/fond_donee.jpg").convert()
@@ -44,11 +46,13 @@ fenetre.blit(button_menu_monstre, (220, 570))
 button_inventaire = pygame.image.load("images/button_inventaire.png").convert_alpha() #l'image sera avec un fond transparant
 fenetre.blit(button_inventaire, (460, 570))
 fond_menu_boutique = pygame.image.load("images/fond_boutique.jpg").convert()
+mod_x2 = pygame.image.load("images/X2.png").convert_alpha()
 
 # creation des zones de click
 clickable_area_boutique = pygame.Rect((30, 570), (160, 49)) # (position)(taille)
 clickable_area_menu_monstre = pygame.Rect((220, 570), (210, 49))
 clickable_area_inventaire = pygame.Rect((460, 570), (168, 49))
+clickable_area_boutique_mod_2 = pygame.Rect((400, 70), (80, 80))
 
 #creation de l'orloge pour afficher l'heure
 horloge = pygame.time.Clock()
@@ -73,6 +77,8 @@ while continuer:
             elif a == 1 and event.type == MOUSEBUTTONUP and event.button == 1 and clickable_area_boutique.collidepoint(event.pos):
                 a = 0
                 print("ferme la boutique")
+            elif a == 1 and event.type == MOUSEBUTTONUP and event.button == 1 and clickable_area_boutique_mod_2.collidepoint(event.pos):
+                print("x2")
         elif event.type == MOUSEBUTTONUP and event.button == 1 and clickable_area_menu_monstre.collidepoint(event.pos):
             print("ouvre menu monstre")
         elif event.type == MOUSEBUTTONUP and event.button == 1 and clickable_area_inventaire.collidepoint(event.pos):
@@ -80,10 +86,12 @@ while continuer:
 
     #permet de reactualiser le fond
     fenetre.blit(fond, (0, 0))
+    fenetre.blit(ombre, (x_ombre, y_ombre))
     fenetre.blit(fond_donee, (0, 0))
     fenetre.blit(button_boutique, (30, 570))
     fenetre.blit(button_menu_monstre, (220, 570))
     fenetre.blit(button_inventaire, (460, 570))
+
     #fenetre.blit(affiche_menu_boutique, ())
     afficher_menu(a, fenetre)
 
